6b9df5c16fb43a090af0ddf40b1cc96854f56587
Aryan Gupta <aryan11gs@gmail.com>
2025-05-02 21:24:22 +0530

    Itema add to cart

--- src/main/java/org/example/Alert/Dismiss.java
+++ src/main/java/org/example/Alert/Dismiss.java
@@ -4,17 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HandilingAlert {
-    public static void main(String[] args) throws InterruptedException {
+public class Dismiss {
+    public static void main(String[] args) {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         driver.findElement(By.id("name")).sendKeys("qwerty");
-        driver.findElement(By.cssSelector("input[value='Alert']")).click();
-        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("input[value='Confirm']")).click();
         System.out.println(driver.switchTo().alert().getText());
-        driver.switchTo().alert().accept();
-
+        driver.switchTo().alert().dismiss();
         driver.quit();
+
     }
 }
