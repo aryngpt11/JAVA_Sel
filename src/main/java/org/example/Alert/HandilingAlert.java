c1c3aa30748540cbc8b5fa7229536fa8eb0e6d41
Aryan Gupta <aryan11gs@gmail.com>
2025-05-02 15:34:07 +0530

    Alertt

--- src/main/java/org/example/Alert/HandilingAlert.java
+++ src/main/java/org/example/Alert/HandilingAlert.java
@@ -1,5 +1,6 @@
 package org.example.Alert;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,5 +9,8 @@ public class HandilingAlert {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("name")).sendKeys("qwerty");
+        driver.findElement(By.cssSelector("input[value='Alert']")).click();
+        driver.quit();
     }
 }
