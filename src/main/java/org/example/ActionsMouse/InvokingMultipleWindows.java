640e0fbb46e56faf058b15afa8db50fd817ef63d
Aryan Gupta <aryan11gs@gmail.com>
2025-06-16 15:12:20 +0530

    Invoking Multiple Windows

--- src/main/java/org/example/ActionsMouse/InvokingMultipleWindows.java
+++ src/main/java/org/example/ActionsMouse/InvokingMultipleWindows.java
@@ -1,15 +1,17 @@
 package org.example.ActionsMouse;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
+import org.apache.commons.io.FileUtils;
+
 
 public class InvokingMultipleWindows {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
@@ -23,7 +25,16 @@ public class InvokingMultipleWindows {
 
         String Coursename=driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
         driver.switchTo().window(PID);
-        driver.findElement(By.cssSelector("[name='name'")).sendKeys(Coursename);
+        WebElement namee=driver.findElement(By.cssSelector("[name='name']"));
+        namee.sendKeys(Coursename);
+        //Taking the ScreenShot
+
+        File file=namee.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file,new File("logo.png"));
+
+        //Height and width
+        System.out.println(namee.getRect().getDimension().getHeight());
+        System.out.println(namee.getRect().getDimension().getWidth());
         Thread.sleep(2000);
 
         driver.quit();
