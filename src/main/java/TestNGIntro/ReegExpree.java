8cb97c5876716e961053c73e6f10930bbad0e87a
Aryan Gupta <aryan11gs@gmail.com>
2025-06-17 13:48:15 +0530

    TestNGRegularexpression

---
+++ src/main/java/TestNGIntro/ReegExpree.java
@@ -0,0 +1,29 @@
+package TestNGIntro;
+
+import org.testng.annotations.Test;
+
+public class ReegExpree {
+    @Test
+    public void WebloginCarLoan(){
+        System.out.println("weblogincar");
+    }
+
+    @Test
+    public void MobileloginCarLoan(){
+        System.out.println("mobilelogincar");
+    }
+
+    @Test
+    public void MobileSigninCarLoan(){
+        System.out.println("mobilesignincar");
+    }
+    @Test
+    public void MobileSignoutCarLoan(){
+        System.out.println("mobilesignoutcar");
+    }
+
+    @Test
+    public void LoginAPICarLoan(){
+        System.out.println("APIcarlogin");
+    }
+}
